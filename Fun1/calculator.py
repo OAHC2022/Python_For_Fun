96b2efd554ce4c60c1347a313ae7630ae76dcf41
Zichao <zichao2018@gmail.com>
2018-10-16 23:10:16 -0400

    I have revised my algorithm when converting an expression to Polish Reverse Notation. Now it should be good to go

--- Fun1/calculator.py
+++ Fun1/calculator.py
@@ -1,7 +1,6 @@
 """Use Polish Reverse Notation to make a simple calculator!!"""
 import sys
 
-
 def binop(string):
     # record the value
     string = string.replace(" ", "").strip()
@@ -12,44 +11,42 @@ def binop(string):
     if string[0] == "-":
         first_value_negative = True
         string = string[1:]
-
     for i, val in enumerate(string):
         if val in "+-*/":
             calc.append(val)
             num.append(string[j:i])
             j = i + 1
     num.append(string[j:])
-
     # Create Reverse Polish Notation
     """
-    my thought on converting to polish notation: count from the last calculation symbol to the first.
-    if I find a "+" or "-", then I extend the previous numbers to the "Reverse_Polish_n" list and then extend the previous calculation symbols.
-    Finally, I extend all the rest to the list.
-    Each block is like: a+b+c*d+ {there is always a addition/subtraction after the mul/div}
+    Revision: my thought on converting to polish notation: count from the last calculation symbol to the first.
+    Always group number with */ togther and calculate their value. Then stack all +- signs in the end
     """
     calc = calc[::-1]
     num = num[::-1]
-    revere_polish_n = []
-    num_j = 0
-    calc_j = 0
+    print(num, calc)
+    revere_polish_n = num.copy()
+    calc_temp = []
+    symbol_num = 0
     for i in range(len(calc)):
-        if calc[i] in "+":
-            revere_polish_n.extend(num[num_j:i + 1])
-            if i > calc_j:
-                revere_polish_n.extend(calc[calc_j:i][::-1])
-            num_j = i + 1
-            calc_j = i
-        if calc[i] in "-":
-            revere_polish_n.extend(num[num_j:i + 1])
-            if i > calc_j:
-                revere_polish_n.extend(calc[calc_j:i][::-1])
-            num_j = i + 1
-            calc_j = i
+        if calc[i] in "*/" and i == len(calc) - 1:
+            calc_temp.append(calc[i])
+            for k in range(len(calc_temp)):
+                revere_polish_n.insert(k + i + 2 + symbol_num, calc_temp[-k])
+        elif calc[i] in "*/":
+            calc_temp.append(calc[i])
+        else:
+            for k in range(len(calc_temp)):
+                revere_polish_n.insert(k + i + 1 + symbol_num, calc_temp[::-1][k])
+            symbol_num += len(calc_temp)
+            calc_temp = []
+    calc_temp = []
+    for i in calc:
+        if i in "+-":
+            calc_temp.append(i)
+    revere_polish_n.extend(calc_temp[::-1])
 
-
-    revere_polish_n.extend(num[num_j:])
-    revere_polish_n.extend(calc[calc_j:][::-1])
-
+    print(revere_polish_n)
 
     # convert the first value to negative if it is negative
     if first_value_negative:
@@ -76,6 +73,7 @@ def binop(string):
             if revere_polish_n[i] == "/":
                 stack_num[-2] = stack_num[-1] / stack_num[-2]
                 stack_num.remove(stack_num[-1])
+    print(revere_polish_n)
     return stack_num[0]
 
 
